6d194af31eca10b160acba0c1b159932eb5315d1
EdricJensenSee <125674757+EdricJensenSee@users.noreply.github.com>
2023-07-16 02:43:05 +0800

    Testing with errors/Progress

--- src/SpecialVendingMachine.java
+++ src/SpecialVendingMachine.java
@@ -1,10 +1,30 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
-* Represents the workings of the special vending machine
-*/ 
+ * Represents the workings of the special vending machine
+ */
 class SpecialVendingMachine extends RegularVendingMachine {
-  public SpecialVendingMachine () {
-	  super();
-  }
+    private ArrayList<String> recipes;
+
+    public SpecialVendingMachine() {
+        super();
+        recipes = new ArrayList<>();
+    }
+
+    private int calculateTotalCalories(ArrayList<String> recipe) {
+        int totalCalories = 0;
+        for (String itemName : recipe) {
+            totalCalories += getItem().getItemCalories().get(itemName);
+        }
+        return totalCalories;
+    }
+
+    public void setRecipes(ArrayList<String> recipe) {
+        recipes = recipe;
+    }
+
+    public ArrayList<String> getRecipes() {
+        return recipes;
+    }
 }
--- src/tester.java
+++ src/tester.java
@@ -1,10 +1,36 @@
+import java.util.ArrayList;
 
 public class tester {
 
 	public static void main(String[] args) {
-		SpecialVendingMachine vn = new SpecialVendingMachine();
-		vn.displayStock();
-		
+	    SpecialVendingMachine vendingMachine = new SpecialVendingMachine();
+	    vendingMachine.getItem().addItem("Chocolate Chip Cookies", 10, 80, 180);
+	    vendingMachine.getItem().addItem("Cream Filling", 6, 10, 200);
+	    vendingMachine.getItem().addItem("Frosting", 8, 10, 150);
+	    vendingMachine.getItem().addItem("Cake", 5, 200, 250);
+	    vendingMachine.getItem().addItem("Chocolate", 10, 50, 220);
+	    vendingMachine.getItem().addItem("Vanilla", 8, 20, 210);
+	    vendingMachine.getItem().addItem("Cream Puffs", 6, 30, 180);
+	    vendingMachine.getItem().addItem("Strawberry", 10, 120, 190);
+	    
+	    ArrayList<String> recipe1 = new ArrayList<>();
+	    recipe1.add("Chocolate Chip Cookies");
+	    recipe1.add("Chocolate");
+	    vendingMachine.setRecipes(recipe1);
+
+	    vendingMachine.prepareProduct("Cookies");
+
+	    ArrayList<String> recipe2 = new ArrayList<>();
+	    recipe2.add("Cream Filling");
+	    recipe2.add("Cream Puffs");
+	    vendingMachine.setRecipes(recipe2);
+
+	    vendingMachine.prepareProduct("Cake");
+	    
+	    vendingMachine.prepareProduct("Cookies");
 	}
 
+
 }
+
+
