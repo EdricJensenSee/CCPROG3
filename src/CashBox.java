65a6b6fafb5f39a782adba126770d106b05ddca8
EdricJensenSee <125674757+EdricJensenSee@users.noreply.github.com>
2023-07-16 15:38:48 +0800

    Split receivePayment method

--- src/CashBox.java
+++ src/CashBox.java
@@ -20,70 +20,79 @@ class CashBox {
         denominations.put(denomination, currentQuantity + quantity);
     }
 
-    public boolean receivePayment(Item item, String itemName, double amountPaid) {
+    private boolean isItemAvailable(Item item, String itemName) {
         int quantity = item.getItemQuantity().getOrDefault(itemName, 0);
-        double price = item.getItemPrice().getOrDefault(itemName, 0.0);
-        int calories = item.getItemCalories().getOrDefault(itemName, 0);
-
         if (quantity <= 0) {
             System.out.println("Item not found in stock.");
-            return true;
+            return false;
         }
+        return true;
+    }
+
+    private void dispenseChange(double change) {
+        System.out.println("Providing change:");
+
+        double[] availableDenominations = {1000.0, 500.0, 200.0, 100.0, 50.0, 20.0, 10.0, 5.0};
+        int[] quantityOfDenom = new int[availableDenominations.length];
+
+        for (int i = 0; i < availableDenominations.length; i++) {
+            double denomination = availableDenominations[i];
+            int num = (int) (change / denomination);
+            if (num > 0) {
+                int numDispensed = Math.min(num, denominations.getOrDefault(denomination, 0));
+                if (numDispensed > 0) {
+                    System.out.println(numDispensed + " ₱" + denomination + " coins/bills");
+                    change -= numDispensed * denomination;
+                    quantityOfDenom[i] = numDispensed;
+                }
+            }
+            if (change == 0) {
+                break;
+            }
+        }
+
+        if (change == 0) {
+            for (int i = 0; i < availableDenominations.length; i++) {
+                double denomination = availableDenominations[i];
+                if (quantityOfDenom[i] > 0) {
+                    denominations.put(denomination, denominations.getOrDefault(denomination, 0) - quantityOfDenom[i]);
+                }
+            }
+        } else {
+            System.out.println("Sorry, the vending machine doesn't have enough change. Change returned.");
+        }
+    }
+
+    public boolean receivePayment(Item item, String itemName, double amountPaid) {
+        if (!isItemAvailable(item, itemName)) {
+            return false;
+        }
+
+        double price = item.getItemPrice().getOrDefault(itemName, 0.0);
 
         if (amountPaid < price) {
             System.out.println("Insufficient payment. Please insert ₱" + (price - amountPaid) + " more.");
             return false;
         } else if (amountPaid > price) {
             double change = amountPaid - price;
-            System.out.println("Providing change:");
+            dispenseChange(change);
+            item.getItemQuantity().put(itemName, item.getItemQuantity().get(itemName) - 1);
+            item.getItemSold().put(itemName, item.getItemSold().getOrDefault(itemName, 0) + 1);
+            item.setTotalSales(item.getTotalSales() + price);
 
-            double[] availableDenominations = {1000.0, 500.0, 200.0, 100.0, 50.0, 20.0, 10.0, 5.0};
-            int[] quantityOfDenom = new int[availableDenominations.length];
-
-            for (int i = 0; i < availableDenominations.length; i++) {
-                double denomination = availableDenominations[i];
-                int num = (int) (change / denomination);
-                if (num > 0) {
-                    int numDispensed = Math.min(num, denominations.getOrDefault(denomination, 0));
-                    if (numDispensed > 0) {
-                        System.out.println(numDispensed + " ₱" + denomination + " coins/bills");
-                        change -= numDispensed * denomination;
-                        quantityOfDenom[i] = numDispensed;
-                    }
-                }
-                if (change == 0) {
-                    break;
-                }
-            }
-
-            if (change == 0) {
-                for (int i = 0; i < availableDenominations.length; i++) {
-                    double denomination = availableDenominations[i];
-                    if (quantityOfDenom[i] > 0) {
-                        denominations.put(denomination, denominations.getOrDefault(denomination, 0) - quantityOfDenom[i]);
-                    }
-                }
-
-                item.getItemQuantity().put(itemName, quantity - 1);
-                item.getItemSold().put(itemName, item.getItemSold().getOrDefault(itemName, 0) + 1);
-                item.setTotalSales(item.getTotalSales() + price);
-
-                System.out.println("Dispensing " + itemName);
-                System.out.println("Change: ₱" + (amountPaid - price));
-                return true;
-            } else {
-                System.out.println("Sorry, the vending machine doesn't have enough change. Change returned.");
-                return true;
-            }
+            System.out.println("Dispensing " + itemName);
+            System.out.println("Change: ₱" + (amountPaid - price));
+            return true;
         } else {
-            item.getItemQuantity().put(itemName, quantity - 1);
+            int calories = item.getItemCalories().getOrDefault(itemName, 0);
+            item.getItemQuantity().put(itemName, item.getItemQuantity().get(itemName) - 1);
             item.getItemSold().put(itemName, item.getItemSold().getOrDefault(itemName, 0) + 1);
             item.setTotalSales(item.getTotalSales() + price);
 
             System.out.println("Dispensing " + itemName + " - Calories: " + calories);
             return true;
         }
-    }
+    }	
 
     public void printReceipt(Item item) {
         System.out.println("Receipt:");
